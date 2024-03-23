c4ec40bd137a62a2fabf363329c2fd8e6404ed28
yvan <https://github.com/Pallies>
2024-03-23 18:21:02 +0100

    add path

---
+++ src/_core/models/enums/path-files.ts
@@ -0,0 +1,7 @@
+export enum PATH_SRC{
+  EMPTY="",
+  INTRO="./assets/videos/generique.mp4",
+  WINNING_POINT="./assets/video/transition_9-points-gagnants.mp4",
+  FOLLOWING="./assets/video/transition_a-la-suite.mp4",
+  FACE_TO_FACE="./assets/video/transition_face-a-face.mp4",
+}
