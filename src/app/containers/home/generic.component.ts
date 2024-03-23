d053c0e808dfb6f1ad9e8774e3e15d60085bd17a
yvan <https://github.com/Pallies>
2024-03-23 17:42:22 +0100

    add component

---
+++ src/app/containers/home/generic.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qpuc-generic',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      generic works!
+    </p>
+  `,
+  styles: ``
+})
+export class GenericComponent {
+
+}
