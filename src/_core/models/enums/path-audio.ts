5c1f2bfed5ea45a3a74a20eef618ba83c8ff9394
yvan <https://github.com/Pallies>
2024-03-23 21:03:24 +0100

    bug path

--- src/_core/models/enums/path-audio.ts
+++ src/_core/models/enums/path-audio.ts
@@ -1,6 +1,7 @@
+
 export enum PATH_AUDIO{
-  BUZZER='./assets/audio/bruitage-buzzer.mp3',
-  POINT_VALIDE='./assets/audio/bruitage-point-valide.mp3',
-  QUALIFIED='./assets/audio/bruitage-qualifie.mp3',
-  DECOMPTE_4_SUITE='./assets/audio/4_a_la_suite_decompte.mp3'
+  BUZZER='@assets/audios/bruitage-buzzer.mp3',
+  POINT_VALIDE='./assets/audios/bruitage-point-valide.mp3',
+  QUALIFIED='./assets/audios/bruitage-qualifie.mp3',
+  DECOMPTE_4_SUITE='./assets/audios/4_a_la_suite_decompte.mp3'
 }
