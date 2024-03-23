4d522342bb7ea397d46fff643b015e9b751c3565
yvan <https://github.com/Pallies>
2024-03-23 21:04:49 +0100

    add intro component

--- src/_core/routes/navigation-winning-point.routes.ts
+++ src/_core/routes/navigation-winning-point.routes.ts
@@ -1,11 +1,14 @@
 import {Routes} from "@angular/router";
 import {PATH_NAME as PATH} from "../models/enums/path-name.enum";
+import {IntroWinningPointsComponent} from "../../app/containers/winning-points/intro-winning-points.component";
 
 export default [
   {
-    path:PATH.INTRO
+    path:PATH.EMPTY,
+    component:IntroWinningPointsComponent
   },
   {
-    path:PATH.EMPTY
+    path:PATH.INTRO,
+    component:IntroWinningPointsComponent
   }
 ]as Routes;
