452ce4df0472d780596b85eeee9df3b9a473b2df
yvan <https://github.com/Pallies>
2024-03-23 21:04:03 +0100

    rename generic

--- src/_core/models/enums/path-name.enum.ts
+++ src/_core/models/enums/path-name.enum.ts
@@ -3,7 +3,7 @@ export enum PATH_NAME{
   EMPTY='',
   HOME='accueil',
   INTRO='intro',
-  GENERIQUE='générique',
+  GENERIC='générique',
   SELECT_PLAYERS='choix-joueurs',
   WINNING_POINTS='points-gagnants'
 }
