6cbd2db20caec27e28d56326cfd9c5f3375e7ac8
yvan <https://github.com/Pallies>
2024-03-23 19:33:50 +0100

    add navigation winning point

---
+++ src/_core/routes/navigation-winning-point.routes.ts
@@ -0,0 +1,11 @@
+import {Routes} from "@angular/router";
+import {PATH_NAME as PATH} from "../models/enums/path-name.enum";
+
+export default [
+  {
+    path:PATH.INTRO
+  },
+  {
+    path:PATH.EMPTY
+  }
+]as Routes;
