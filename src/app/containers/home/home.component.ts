1dfc360d7557484ce33b362f491426dbf637115d
yvan <https://github.com/Pallies>
2024-03-23 13:13:00 +0100

    nav to page

--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,26 +1,33 @@
 import { Component } from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     MatButton,
-    MatIcon
+    MatIcon,
+    RouterLink
   ],
   template: `
     <div class="container">
       <div class="title-box">
 
-      @for (char of title.split(''); track $index) {
-        <h1 class="title-char" [id]="'char-'+$index">{{ char }}</h1>
-      }
+        @for (char of title.split(''); track $index) {
+          <h1 class="title-char" [id]="'char-'+$index">{{ char }}</h1>
+        }
       </div>
       <div class="button-box">
-      <button class="button-action" mat-raised-button color="accent">Start
-        <span><mat-icon class="mat-icon-rtl-mirror icon" fontIcon="navigate_next"/></span>
-      </button>
+        <button class="button-action"
+                mat-raised-button color="accent"
+                routerLink="/home/select-players">
+          Start
+          <span>
+          <mat-icon class="mat-icon-rtl-mirror icon" fontIcon="navigate_next"/>
+        </span>
+        </button>
 
       </div>
     </div>
