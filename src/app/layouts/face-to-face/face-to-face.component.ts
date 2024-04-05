073a4e27c6739f3ac03e1847317cba5e7dd256be
yvan <https://github.com/Pallies>
2024-04-05 18:07:31 +0200

    add face to face component

--- src/app/layouts/face-to-face/face-to-face.component.ts
+++ src/app/layouts/face-to-face/face-to-face.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {RectangleComponent} from "../../../shared/components/rectangle/rectangle.component";
+import {PlayerCardComponent} from "../../../shared/components/player-card/player-card.component";
+import {PlayerStoreService} from "../../../shared/store/player-store.service";
 
 @Component({
   selector: 'qpuc-face-to-face',
   standalone: true,
-  imports: [],
+  imports: [
+    RectangleComponent,
+    PlayerCardComponent
+  ],
   templateUrl: './face-to-face.component.html',
   styleUrl: './face-to-face.component.scss'
 })
 export class FaceToFaceComponent {
+  $storePlayers = inject(PlayerStoreService);
+  players = this.$storePlayers.players;
 
 }
