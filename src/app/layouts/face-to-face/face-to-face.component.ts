13798fdb9e309fbe471f822f35308147426f7cc5
yvan <https://github.com/Pallies>
2024-04-05 23:22:06 +0200

    decrement timer

--- src/app/layouts/face-to-face/face-to-face.component.ts
+++ src/app/layouts/face-to-face/face-to-face.component.ts
@@ -1,20 +1,50 @@
-import {Component, inject} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  computed,
+  inject,
+  QueryList,
+  Signal,
+  ViewChildren,
+  WritableSignal
+} from '@angular/core';
 import {RectangleComponent} from "../../../shared/components/rectangle/rectangle.component";
 import {PlayerCardComponent} from "../../../shared/components/player-card/player-card.component";
 import {PlayerStoreService} from "../../../shared/store/player-store.service";
+import {MatButtonModule} from "@angular/material/button";
+import {CountDownService} from "./count-down.service";
+import {FaceToFaceAudioService} from "./face-to-face-audio.service";
+
 
 @Component({
   selector: 'qpuc-face-to-face',
   standalone: true,
   imports: [
     RectangleComponent,
-    PlayerCardComponent
+    PlayerCardComponent,
+    MatButtonModule
   ],
+  providers: [CountDownService],
   templateUrl: './face-to-face.component.html',
   styleUrl: './face-to-face.component.scss'
 })
-export class FaceToFaceComponent {
+export class FaceToFaceComponent implements AfterViewInit {
   $storePlayers = inject(PlayerStoreService);
+  $countDown = inject(CountDownService);
   players = this.$storePlayers.players;
+  buttonName: Signal<string> = computed(() => this.$countDown._starter() ? "Stop":"Commencez" )
 
+  @ViewChildren('rectangle') rectangles!: QueryList<RectangleComponent>
+
+  pursuit(index: number) {
+    this.$countDown.currentIndexDecrement(index)
+  }
+
+  ngAfterViewInit(): void {
+    this.rectangles?.forEach(el => this.$countDown.setComponent(el));
+  }
+
+  start() {
+    this.$countDown.start()
+  }
 }
--- src/shared/components/rectangle/rectangle.directive.ts
+++ src/shared/components/rectangle/rectangle.directive.ts
@@ -1,39 +1,40 @@
-import {
-  Directive,
-  ElementRef, EventEmitter,
-  inject,
-  Input,
-  input,
-  OnChanges, Output,
-  Renderer2,
-  SimpleChanges,
-  ViewChild
-} from '@angular/core';
+import {Directive, ElementRef, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 
 @Directive({
-  selector: '[qpucRectangle]',
-  standalone: true
+    selector: '[qpucRectangle]',
+    standalone: true,
 })
 export class RectangleDirective implements OnChanges {
+    @Input() qpucRectangle!: boolean;
+    @Output() next: EventEmitter<boolean> = new EventEmitter<boolean>()
+    height: number = 100;
+    el: ElementRef;
+    timerId!: number;
 
-  @Input() qpucRectangle!: boolean;
-  @Output() next = new EventEmitter<void>()
-  height: number = 100;
-  el = inject(ElementRef)
-  timerId: any;
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (this.qpucRectangle) {
-
-      this.timerId = setInterval(() => {
-        if (this.height <= 0)
-          return this.next.emit()
-
-        this.el.nativeElement.style.setProperty('--height-after', (--this.height + '%'));
-      }, 100);
-    } else {
-      if (this.timerId)
-        clearInterval(this.timerId);
+    constructor(private elementRef: ElementRef) {
+        this.el = elementRef;
     }
-  }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.qpucRectangle && this.height ? this.startTimer() : this.cancelTimer();
+    }
+
+    private startTimer() {
+        this.timerId = setInterval(() => {
+            if (this.height <= 0) {
+                clearInterval(this.timerId);
+                this.next.emit(true);
+            }
+            this.el.nativeElement.style.setProperty('--height-after', (--this.height + '%'));
+        }, 100);
+    }
+
+    private cancelTimer() {
+        if (this.timerId) {
+            window.clearInterval(this.timerId);
+        }
+        if(this.height < 0) {
+          this.next.emit(true);
+        }
+    }
 }
