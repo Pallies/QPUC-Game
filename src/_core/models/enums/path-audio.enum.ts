b46b2abaadb12eb05c9a1d88477844aaa0140515
yvan <https://github.com/Pallies>
2024-04-19 08:13:01 +0200

    false answer

--- src/_core/models/enums/path-audio.enum.ts
+++ src/_core/models/enums/path-audio.enum.ts
@@ -1,6 +1,7 @@
 
 export enum PATH_AUDIO{
   BUZZER='./assets/audios/bruitage-buzzer.mp3',
+  FALSE_ANSWER='./assets/audios/bruitage-faux.mp3',
   POINT_VALIDE='./assets/audios/bruitage-point-valide.mp3',
   QUALIFIED='./assets/audios/bruitage-qualifie.mp3',
   DECREMENT='./assets/audios/4_a_la_suite_decompte.mp3'
