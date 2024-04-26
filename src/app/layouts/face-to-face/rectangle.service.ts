25ba6cac58f4170e73d8a677aeab590c30dcd68f
yvan <https://github.com/Pallies>
2024-04-26 10:09:43 +0200

    manage rectangle

---
+++ src/app/layouts/face-to-face/rectangle.service.ts
@@ -0,0 +1,34 @@
+import { Injectable} from "@angular/core";
+import {RectangleComponent} from "../../../shared/components/rectangle/rectangle.component";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RectangleService {
+
+  private _component: RectangleComponent[] = []
+
+
+  setComponent(component: RectangleComponent) {
+    this._component.push(component)
+  }
+
+
+  activate(index: number ) {
+    if (index >= 0) {
+      this._component[index].active = true
+    }
+  }
+
+  stop(index:number) {
+    this._component[index].active = false
+  }
+
+  init() {
+    this._component
+      .forEach(c => {
+        c.active = undefined
+      })
+  }
+}
